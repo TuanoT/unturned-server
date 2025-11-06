ab6667be8622f714b09ffcb5d501bb28943f4405
Tom Purcell <tommaaspurcell@gmail.com>
2025-11-06 15:24:03 +1100

    Nuked Repo

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Update server on start
-/update.sh
-
-cd "$UNTURNED_DIR"
-echo "Starting Unturned server..."
-mono Unturned.exe -nographics -${MAP} -${MODE} -nosync $( [ "$PVE" = "true" ] && echo "-pve" || echo "-pvp" ) -players:$PLAYERS -sv
