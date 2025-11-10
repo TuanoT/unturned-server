025a23dd3c354b717e1b58b2c5ae6d2deea40810
TomMaasPurcell <s3945185@student.rmit.edu.au>
2025-11-10 18:33:41 +1100

    removed dumb comments

--- init.sh
+++ init.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-# Set default values if not provided
 SERVER_NAME="${SERVER_NAME:-MelbourneVanilla}"
 MAP="${MAP:-Washington}"
 GSLT="${GSLT:-}"
 
-# Build command
 CMD="./Unturned_Headless.x86_64 -nographics -batchmode +InternetServer/$SERVER_NAME +Map:$MAP"
 
 # Add GSLT if provided
@@ -14,5 +12,4 @@ if [ -n "$GSLT" ]; then
     CMD="$CMD +GSLT:$GSLT"
 fi
 
-# Execute the server
 exec $CMD
